bce30c10c40c60b129e3e0e258b953ae5cb57046
xaatw0 <xaatw0@gmail.com>
2025-02-16 05:05:37 +0900

    AdditionalInformationNodeを追加

---
+++ ollama_talk_server/lib/src/domain/commands/nodes/additional_information_node.dart
@@ -0,0 +1,25 @@
+import 'package:ollama_talk_server/src/domain/commands/llm_command.dart';
+
+import '../command.dart';
+import '../llm_models/llm_model.dart';
+
+class AdditionalInformationNode implements ICommand<String, String> {
+  const AdditionalInformationNode(
+    this.model,
+    this.commandForInformation,
+  );
+
+  final ILlmModel model;
+  final LlmCommand commandForInformation;
+
+  @override
+  Future<String> execute(String data) async {
+    final additionalInformation = await commandForInformation.execute(data);
+
+    final prompt = '$data\n--------------\n'
+        'answer to the above, based on the following information:\n$additionalInformation';
+
+    final response = await model.execute(prompt);
+    return response;
+  }
+}
---
+++ ollama_talk_server/test/domain/commands/nodes/additional_information_node_test.dart
@@ -0,0 +1,27 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:ollama_talk_server/src/domain/commands/llm_command.dart';
+import 'package:ollama_talk_server/src/domain/commands/llm_models/gemini_model.dart';
+import 'package:ollama_talk_server/src/domain/commands/nodes/additional_information_node.dart';
+import 'package:test/test.dart';
+
+import 'additional_information_node_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<LlmCommand>()])
+void main() {
+  test('AdditionalInformationNode', () async {
+    final prompt = '坂本龍馬の誕生日は西暦で何年ですか(数字のみ)';
+
+    final mockRag = MockLlmCommand();
+    when(mockRag.execute(prompt)).thenAnswer((_) =>
+        '坂本 龍馬（さかもと りょうま、天保6年11月15日〈1836年1月3日〉 - 慶応3年11月15日〈1867年12月10日〉）は、日本の幕末の土佐藩士、志士、経営者。');
+
+    final target = AdditionalInformationNode(
+      GeminiModel(),
+      mockRag,
+    );
+
+    final response = await target.execute(prompt);
+    expect(response.trim(), '1836');
+  });
+}
